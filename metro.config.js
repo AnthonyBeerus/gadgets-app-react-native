79887d9a6a99fc08615cc45061e36549c906a975
Anthony Morima jr <anthony.morima@gmail.com>
2026-01-26 14:48:59 +0200

    chore: exclude tests from metro bundler

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,12 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
-// Block whatwg-fetch and use cross-fetch instead
-config.resolver.blockList = [/node_modules\/whatwg-fetch\/.*/];
+// Block whatwg-fetch AND exclude test files from bundling
+config.resolver.blockList = [
+  /node_modules\/whatwg-fetch\/.*/,
+  /.*\/__tests__\/.*/,
+  /.*\.test\.(ts|tsx|js|jsx)/,
+];
 
 config.resolver.extraNodeModules = {
   "whatwg-fetch": require.resolve("cross-fetch"),
